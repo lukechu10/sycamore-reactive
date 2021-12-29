36f9e1cb6279a3a053b096d334e1510afb02c23c
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-12-29 13:37:27 -0800

    Make lifetimes work

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,14 +3,26 @@ use sycamore_reactive::*;
 fn main() {
     create_scope(|ctx| {
         let data = ctx.create_signal(123);
-        dbg!(data.get());
-        data.set(456);
-        dbg!(data.get());
-
-        create_scope(|ctx| {
-            let inner = ctx.create_signal("abc");
-            dbg!(inner.get());
+        ctx.create_effect(|| {
+            println!("Hello World!");
+            // ctx.create_effect(|| {
+            //     dbg!(data.get());
+            // });
             dbg!(data.get());
+        });
+
+        ctx.on_cleanup(|| {
+            println!("Outer scope cleanup");
+        });
+
+        ctx.create_child_scope(|ctx| {
+            let inner = ctx.create_signal("abc");
+            ctx.on_cleanup(|| {
+                println!("Start inner scope cleanup");
+                dbg!(inner.get());
+                dbg!(data.get());
+                println!("Finish inner scope cleanup");
+            })
         });
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,74 @@
+use std::any::Any;
 use std::cell::RefCell;
-use std::marker::PhantomData;
 use std::rc::Rc;
 
 /// Reactive context.
-pub struct Ctx {}
+#[derive(Default)]
+pub struct Ctx<'a> {
+    signals: RefCell<Vec<Box<dyn Any>>>,
+    effects: RefCell<Vec<Box<dyn Fn() + 'a>>>,
+    cleanups: RefCell<Vec<Box<dyn FnOnce() + 'a>>>,
+}
 
-pub struct Signal<'a, T> {
-    phantom: PhantomData<&'a ()>,
+pub struct Signal<T: 'static> {
     value: RefCell<Rc<T>>,
 }
 
-pub fn create_scope(mut f: impl FnMut(&Ctx)) {
-    let ctx = Ctx {};
+trait AnySignal: Any {}
+
+impl<T> AnySignal for Signal<T> {}
+
+pub fn create_scope<'a, F>(f: F)
+where
+    F: FnOnce(&Ctx<'a>),
+{
+    let ctx = Ctx::default();
     f(&ctx);
 }
 
-impl Ctx {
-    pub fn create_signal<T>(&self, value: T) -> Signal<T> {
-        Signal {
-            phantom: PhantomData,
+impl<'a> Ctx<'a> {
+    pub fn create_signal<T>(&self, value: T) -> &'a Signal<T> {
+        let signal = Signal {
             value: RefCell::new(Rc::new(value)),
+        };
+        let boxed = Box::new(signal);
+        let ptr = boxed.as_ref() as *const Signal<T>;
+        self.signals.borrow_mut().push(boxed);
+        // SAFETY: the address of the Signal<T> lives as long as 'a because:
+        // - It is allocated on the heap and therefore has a stable address.
+        // - self.signals is append only. That means that the Box<Signal<T>> will not be dropped until Self is dropped.
+        unsafe { &*ptr }
+    }
+
+    pub fn create_effect(&self, f: impl Fn() + 'a) {
+        f(); // TODO
+        self.effects.borrow_mut().push(Box::new(f));
+    }
+
+    pub fn on_cleanup(&self, f: impl FnOnce() + 'a) {
+        self.cleanups.borrow_mut().push(Box::new(f));
+    }
+
+    pub fn create_child_scope<F>(&self, f: F)
+    where
+        F: FnOnce(&Ctx),
+    {
+        let ctx = Ctx::default();
+        f(&ctx);
+    }
+}
+
+impl Drop for Ctx<'_> {
+    fn drop(&mut self) {
+        // Call cleanup functions.
+        let cleanups = self.cleanups.take();
+        for cb in cleanups {
+            cb();
         }
     }
 }
 
-impl<'a, T> Signal<'a, T> {
+impl<T> Signal<T> {
     pub fn get(&self) -> Rc<T> {
         self.value.borrow().clone()
     }
