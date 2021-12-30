8eb491ed12164476d9d1bfbcc71ce692a77b6064
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-12-30 08:26:21 -0800

    Fix stacked borrows

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,13 +1,15 @@
 use sycamore_reactive::*;
 
 fn main() {
-    create_scope(|ctx| {
+    let mut outer = None;
+    let scope = create_scope(|ctx| {
+        outer = Some(ctx);
         let data = ctx.create_signal(123);
         ctx.create_effect(|| {
             println!("Hello World!");
-            // ctx.create_effect(|| {
-            //     dbg!(data.get());
-            // });
+            ctx.create_effect(|| {
+                dbg!(data.get());
+            });
             dbg!(data.get());
         });
 
@@ -15,7 +17,7 @@ fn main() {
             println!("Outer scope cleanup");
         });
 
-        ctx.create_child_scope(|ctx| {
+        create_scope(|ctx| {
             let inner = ctx.create_signal("abc");
             ctx.on_cleanup(|| {
                 println!("Start inner scope cleanup");
@@ -25,4 +27,7 @@ fn main() {
             })
         });
     });
+    drop(scope);
+    // let c = outer.unwrap();
+    // c.create_effect(|| {});
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
 use std::any::Any;
 use std::cell::RefCell;
+use std::marker::PhantomPinned;
+use std::pin::Pin;
 use std::rc::Rc;
+
+pub struct Scope<'a> {
+    ctx: Pin<Box<Ctx<'a>>>,
+}
 
 /// Reactive context.
 #[derive(Default)]
 pub struct Ctx<'a> {
-    signals: RefCell<Vec<Box<dyn Any>>>,
-    effects: RefCell<Vec<Box<dyn Fn() + 'a>>>,
+    effects: RefCell<Vec<Box<dyn FnMut() + 'a>>>,
     cleanups: RefCell<Vec<Box<dyn FnOnce() + 'a>>>,
+    // Ctx owns the raw pointers in the Vec.
+    signals: RefCell<Vec<*mut dyn Any>>,
+    _pinned: PhantomPinned,
 }
 
 pub struct Signal<T: 'static> {
@@ -18,29 +26,34 @@ trait AnySignal: Any {}
 
 impl<T> AnySignal for Signal<T> {}
 
-pub fn create_scope<'a, F>(f: F)
+pub fn create_scope<'a, F>(f: F) -> Scope<'a>
 where
-    F: FnOnce(&Ctx<'a>),
+    F: FnOnce(&'a Ctx<'a>),
 {
     let ctx = Ctx::default();
-    f(&ctx);
+    let scope = Scope { ctx: Box::pin(ctx) };
+    // TODO: very unsafe
+    // SAFETY: not
+    f(unsafe { &*(scope.ctx.as_ref().get_ref() as *const Ctx<'a>) });
+    scope
 }
 
 impl<'a> Ctx<'a> {
-    pub fn create_signal<T>(&self, value: T) -> &'a Signal<T> {
+    pub fn create_signal<T>(&self, value: T) -> &Signal<T> {
         let signal = Signal {
             value: RefCell::new(Rc::new(value)),
         };
         let boxed = Box::new(signal);
-        let ptr = boxed.as_ref() as *const Signal<T>;
-        self.signals.borrow_mut().push(boxed);
+        let ptr = Box::into_raw(boxed);
+        self.signals.borrow_mut().push(ptr);
         // SAFETY: the address of the Signal<T> lives as long as 'a because:
         // - It is allocated on the heap and therefore has a stable address.
         // - self.signals is append only. That means that the Box<Signal<T>> will not be dropped until Self is dropped.
+        // TODO: this is a violation of StackedBorrows when dropped.
         unsafe { &*ptr }
     }
 
-    pub fn create_effect(&self, f: impl Fn() + 'a) {
+    pub fn create_effect(&self, mut f: impl FnMut() + 'a) {
         f(); // TODO
         self.effects.borrow_mut().push(Box::new(f));
     }
@@ -61,9 +74,17 @@ impl<'a> Ctx<'a> {
 impl Drop for Ctx<'_> {
     fn drop(&mut self) {
         // Call cleanup functions.
-        let cleanups = self.cleanups.take();
-        for cb in cleanups {
+        for cb in self.cleanups.take() {
             cb();
+        }
+        // Drop effects.
+        drop(self.effects.take());
+        // Drop signals.
+        for i in self.signals.take() {
+            // SAFETY: These pointers were allocated in Self::create_signal.
+            unsafe {
+                drop(Box::from_raw(i));
+            }
         }
     }
 }
